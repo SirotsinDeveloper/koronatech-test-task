29b9f923efc2524d1f84d3debe639027e9c96cee
Viacheslav Sirotsin <slava.sirotin.02@mail.ru>
2025-02-01 14:47:57 +0300

    reate new method, without validate

--- src/main/java/by/slava/test/koronashift/task/service/impl/PersonService.java
+++ src/main/java/by/slava/test/koronashift/task/service/impl/PersonService.java
@@ -1,5 +1,7 @@
 package by.slava.test.koronashift.task.service.impl;
 
+import by.slava.test.koronashift.task.model.Employee;
+import by.slava.test.koronashift.task.model.Manager;
 import by.slava.test.koronashift.task.service.PersonServiceInterface;
 
 import java.util.List;
@@ -7,7 +9,14 @@ import java.util.List;
 public class PersonService implements PersonServiceInterface {
     @Override
     public void processData(List<String> lines) {
-
+        for (String line : lines) {
+            String[] parts = line.split(",");
+            if (parts[0].equals("Manager")) {
+                Manager manager = new Manager(parts[0], Integer.parseInt(parts[1]), parts[2], Double.parseDouble(parts[3]), parts[4]);
+            } else if (parts[0].equals("Employee")) {
+                Employee employee = new Employee(parts[0], Integer.parseInt(parts[1]), parts[2], Double.parseDouble(parts[3]), Integer.parseInt(parts[4]));
+            }
+        }
     }
 
     @Override
