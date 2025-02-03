d02c5d345725f10a1274a5b800298c66941415fe
Viacheslav Sirotsin <slava.sirotin.02@mail.ru>
2025-02-03 18:27:43 +0300

    written a method for processing a file

--- src/main/java/by/slava/test/koronashift/task/app/Main.java
+++ src/main/java/by/slava/test/koronashift/task/app/Main.java
@@ -2,5 +2,6 @@ package by.slava.test.koronashift.task.app;
 
 public class Main {
     public static void main(String[] args) {
+
     }
 }
--- src/main/java/by/slava/test/koronashift/task/service/impl/PersonService.java
+++ src/main/java/by/slava/test/koronashift/task/service/impl/PersonService.java
@@ -4,17 +4,36 @@ import by.slava.test.koronashift.task.model.Employee;
 import by.slava.test.koronashift.task.model.Manager;
 import by.slava.test.koronashift.task.service.PersonServiceInterface;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PersonService implements PersonServiceInterface {
+    private List<Employee> employees = new ArrayList<>();
+    private List<Manager> managers = new ArrayList<>();
+    private List<String> invalidData = new ArrayList<>();
+
     @Override
     public void processData(List<String> lines) {
         for (String line : lines) {
             String[] parts = line.split(",");
-            if (parts[0].equals("Manager")) {
-                Manager manager = new Manager(parts[0], Integer.parseInt(parts[1]), parts[2], Double.parseDouble(parts[3]), parts[4]);
-            } else if (parts[0].equals("Employee")) {
-                Employee employee = new Employee(parts[0], Integer.parseInt(parts[1]), parts[2], Double.parseDouble(parts[3]), Integer.parseInt(parts[4]));
+            if (parts.length == 5) {
+                try {
+                    if ("manager".equalsIgnoreCase(parts[0])) {
+                        Manager manager = new Manager(parts[0], Integer.parseInt(parts[1]), parts[2],Double.parseDouble(parts[3]), parts[4]);
+                        managers.add(manager);
+                    } else if ("employee".equalsIgnoreCase(parts[0])) {
+                        Employee employee = new Employee(parts[0], Integer.parseInt(parts[1]), parts[2],Double.parseDouble(parts[3]), Integer.parseInt(parts[4]));
+                        if (employee.getSalary() > 0) {
+                            employees.add(employee);
+                        } else {
+                            invalidData.add(line);
+                        }
+                    }
+                } catch (NumberFormatException e) {
+                    invalidData.add(line);
+                }
+            } else {
+                invalidData.add(line);
             }
         }
     }
